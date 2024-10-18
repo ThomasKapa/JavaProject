ef18a50c4badd546b5ae88027680f62cab5e1bbe
xtkaravas <xtkaravas@ote.gr>
2024-10-18 09:17:55 +0300

    second commit, starting building class customer.

--- src/com/travelcompany/eshop/domain/Customer.java
+++ src/com/travelcompany/eshop/domain/Customer.java
@@ -1,4 +1,41 @@
 package com.travelcompany.eshop.domain;
 
+import com.travelcompany.eshop.enumeration.Category;
+import com.travelcompany.eshop.enumeration.Nationality;
+
 public class Customer {
+
+    private Long id ;
+    private String customerName;
+    private String email;
+    private String address;
+    private String Nationality;
+    private Nationality nationality;
+    private Category category;
+
+
+    public Customer (Long id, String customerName, String email, String address, Nationality nationality, Category category){
+        this.id = id;
+        this.customerName = customerName;
+        this.email = email;
+        this.address = address;
+        this.nationality = nationality;
+        this.category = category;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
